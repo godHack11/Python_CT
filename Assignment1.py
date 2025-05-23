3fbb54995965db70dfb582d18ba376a0e1a69add
Shubham Bargart <129400218+godHack11@users.noreply.github.com>
2025-05-24 00:46:10 +0530

    Create Assignment1.py

---
+++ Assignment1.py
@@ -0,0 +1,19 @@
+# Create lower triangular, upper triangular and pyramid contaning the "*" character.
+
+rows = 5
+
+print("Lower Triangular Pattern:")
+for i in range(1, rows + 1):
+    print("*" * i)
+
+
+print("\nUpper Triangular Pattern:")
+for i in range(rows, 0, -1):
+    print("*" * i)    
+
+
+print("\nPyramid Pattern:")
+for i in range(1, rows + 1):
+    print(" " * (rows - i) + "*" * (2 * i - 1))    
+
+
