e5b8431acbb148effd3ed333e6745e804d857602
Shubham Bargart <129400218+godHack11@users.noreply.github.com>
2025-05-29 23:49:29 +0530

    Create Week-2Venv.py

---
+++ Week-2Venv.py
@@ -0,0 +1,9 @@
+# Virtual Environments and Packages
+# Creating Virtual Environments
+python -m venv tutorial-env
+
+# Once you’ve created a virtual environment, you may activate it.
+# For mac -  source tutorial-env/bin/activate
+
+# Managing Packages with pip
+python -m pip install novas
